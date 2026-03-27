3fb9a8711c9bf89cbf23a8fdd3c5807617014315
Josh Pine <joshua.pine@crpusd.org>
2026-03-27 10:12:57 -0700

    All stats work

--- stat_sheet.py
+++ stat_sheet.py
@@ -114,68 +114,57 @@ Left_team_stats = {}
 for player in Left_team_player_list:
     Left_team_stats[player] = {"pts": 0,"reb": 0, "assists":0}
 
-def Chose_player(win, player_ind, stat):
+def Chose_player(win, player_ind, stat, points):
     #Uses player's index to find their actual jersey number
     player_num = Left_team_player_list[player_ind]
     t = True
     if stat != "pts":
         Left_team_stats[player_num][stat] += 1
     else:
-        while t == True:
-        #Accounting for freethrows, midrange, 3-pt shots
-            shot = input("1, 2, or 3 pointer? ")
-            if shot == "1" or shot == "2" or shot == "3":
-                Left_team_stats[player_num][stat] += int(shot)
-                t = False
-            else: 
-                print("Enter either 1, 2, or 3")
+        Left_team_stats[player_num][stat] += int(points)
     print("Player" + " " + str(player_num) + " " + stat + ": " + str(Left_team_stats[player_num][stat]))
     close_new(win)
-    
-    
-    
+
 #Creates player buttons
-def create_player_buttons(new_window,stat):
-    Player1_button = tk.Button(new_window, text = Left_team_player_list[0], command=lambda: Chose_player(new_window,0,stat))
-    Player2_button = tk.Button(new_window, text = Left_team_player_list[1], command=lambda: Chose_player(new_window,1,stat))
-    Player3_button = tk.Button(new_window, text = Left_team_player_list[2], command=lambda: Chose_player(new_window,2,stat))
-    Player4_button = tk.Button(new_window, text = Left_team_player_list[3], command=lambda: Chose_player(new_window,3,stat))
-    Player5_button = tk.Button(new_window, text = Left_team_player_list[4], command=lambda: Chose_player(new_window,4,stat))
+def create_player_buttons(new_window,stat,points):
+    Player1_button = tk.Button(new_window, text = Left_team_player_list[0], command=lambda: Chose_player(new_window,0,stat,points))
+    Player2_button = tk.Button(new_window, text = Left_team_player_list[1], command=lambda: Chose_player(new_window,1,stat,points))
+    Player3_button = tk.Button(new_window, text = Left_team_player_list[2], command=lambda: Chose_player(new_window,2,stat,points))
+    Player4_button = tk.Button(new_window, text = Left_team_player_list[3], command=lambda: Chose_player(new_window,3,stat,points))
+    Player5_button = tk.Button(new_window, text = Left_team_player_list[4], command=lambda: Chose_player(new_window,4,stat,points))
     Player1_button.pack()
     Player2_button.pack()
     Player3_button.pack()
     Player4_button.pack()
     Player5_button.pack()
 #Opens interface with player numbers
-def open_number_interface(stat):
+def open_number_interface(stat, points):
     new_window = open_new_interface()
-    create_player_buttons(new_window,stat)
+    create_player_buttons(new_window,stat,points)
     
 #Create buttons for next interface, but keep hidden
-Rebound_button = tk.Button(Left_team, text = "Rebound", command=lambda: open_number_interface("reb"))
-Assist_button = tk.Button(Left_team, text = "Assist", command=lambda: open_number_interface("assists"))
-Point_button = tk.Button(Left_team, text = "Point", command=lambda: open_number_interface("pts"))
+Rebound_button = tk.Button(Left_team, text = "Rebound", command=lambda: open_number_interface("reb", 0))
+Assist_button = tk.Button(Left_team, text = "Assist", command=lambda: open_number_interface("assists", 0))
 Finish_button = tk.Button(Left_team, text = "Finish", command=lambda: end_program())
 
 #Points have their own listbox
-leftpt_listbox = tk.Listbox(Left_team, height=6, width=30)
+leftpt_listbox = tk.Listbox(Left_team, height=3, width=30)
 pt_options = ["1","2","3"]
 for options in pt_options:
     leftpt_listbox.insert(tk.END, options)
     
 def get_from_leftpt_listbox():
-    sel = left_listbox.curselection()
+    sel = leftpt_listbox.curselection()
     if not sel:
         return None
     return leftpt_listbox.get(sel[0]).lower()
 
 
 def applyptselection():
-    color = getfromrightlistbox()
-    if color:
-        Right_team.config(bg=color)
+    points = get_from_leftpt_listbox()
+    open_number_interface("pts", points)
 
-Apply_Button_pt = tk.Button(Left_team, text="Done selecting", command=applyptselection)
+Apply_Button_pt = tk.Button(Left_team, text="Done selecting points", command=applyptselection)
 
 #Clear GUI of everything so far (Procedure)
 
@@ -186,10 +175,11 @@ def cleareverything(list):
         Completed_color_button.destroy()
         
         #Show buttons after click of completed color button
-        Rebound_button.pack(side=tk.LEFT)
-        Finish_button.pack(side=tk.BOTTOM)
         leftpt_listbox.pack(side=tk.TOP)
         Apply_Button_pt.pack(side=tk.TOP)
+        Assist_button.pack(side=tk.TOP)
+        Rebound_button.pack(side=tk.TOP)
+        Finish_button.pack(side=tk.TOP)
     else: 
         something()
 
